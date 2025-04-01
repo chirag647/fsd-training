db9d90db64ee72e0e7906b1e1ed23b414cd76707
chirag647 <154801116+chirag647@users.noreply.github.com>
2025-04-01 10:13:58 +0530

    Add files via upload

---
+++ add.js
@@ -0,0 +1,10 @@
+const add=(a,b)=>{
+    return a+b;
+}
+const sub=(a,b)=>{
+    return a-b;
+}
+export default add;
+export {sub};
+//module.exports=sub
+//console.log(add(2,3))
---
+++ index.js
@@ -0,0 +1,3 @@
+import add,{sub} from "./add.js"
+console.log(add(3,7));
+console.log(sub(5,6));
